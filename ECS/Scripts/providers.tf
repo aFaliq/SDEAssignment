b82fa12beb77bf32a4027d859d6a1450f4547f3e
aFaliq <amir.faliq92@gmail.com>
2024-08-23 14:25:20 +0800

    regenerate new secret

--- ECS/Scripts/providers.tf
+++ ECS/Scripts/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = "AKIAQ4NSBED5OXJQHR44"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
