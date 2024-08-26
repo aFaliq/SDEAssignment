ea58d17dcab9a40e8455e217b88374f48347f0f4
aFaliq <amir.faliq92@gmail.com>
2024-08-26 14:34:36 +0800

    Update main.tf

--- ECS/Scripts/main.tf
+++ ECS/Scripts/main.tf
@@ -1,11 +1,6 @@
 data "aws_ecr_repository" "my_repo" {
   name = var.repository
 }
-
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
-}
-
 resource "aws_ecs_cluster" "my_cluster" {
   name = var.ecs_cluster_name
 }
@@ -16,7 +11,6 @@ resource "aws_ecs_task_definition" "my_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.ecs_task_cpu
   memory                   = var.ecs_task_memory
-  execution_role_arn       = "${data.aws_iam_role.ecs_task_execution_role.arn}"
 
   container_definitions = jsonencode([
     {
